ab8f5235004efc8674f57d33e0ed7cb3aad0c1be
savokiss <jaynaruto@qq.com>
2022-11-07 16:50:23 +0800

    fix: gtag call

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,8 +5,8 @@ module.exports = {
     ['link', { rel: 'icon', href: `/logo.png` }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['script', {src: 'https://www.googletagmanager.com/gtag/js?id=G-SFSR5RSG46'}],
-    ['script', {}, `G-SFSR5RSG46`]
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-SFSR5RSG46' }],
+    ['script', {}, `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-SFSR5RSG46');`]
   ],
   themeConfig: {
     repo: 'savokiss/docs',
